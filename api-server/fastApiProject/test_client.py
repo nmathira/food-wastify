69f1532e1b963b13cf950ccb0fbfc9af6d62ddfb
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2024-11-08 23:45:50 -0500

    Added parallax effect to background image

--- api-server/fastApiProject/test_client.py
+++ api-server/fastApiProject/test_client.py
@@ -5,6 +5,7 @@ frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 data = camera.read()
-if data[0]:
-    cv2.imshow("asdf",data[1])
 
+cv2.imshow("asdf",data[1])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
