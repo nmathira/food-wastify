6febd086573ea1fe6dc17c3c4cb806d73f6ba5da
nmathira <mathirajanniranjan@gmail.com>
2024-11-08 23:46:09 -0500

    add POST to test script

---
+++ api-server/fastApiProject/readme.md
@@ -0,0 +1,1 @@
+curl -X POST "http://127.0.0.1:8000/upload-food" -F "file=@/home/niranjan/pictures/wallpapers/s1.png"
--- api-server/fastApiProject/test_client.py
+++ api-server/fastApiProject/test_client.py
@@ -1,10 +1,16 @@
-import cv2
+import io
 
+import cv2
+import os
+import requests
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
 camera = cv2.VideoCapture(0)
 frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-data = camera.read()
-if data[0]:
-    cv2.imshow("asdf",data[1])
-
+while True:
+    data = camera.read()
+    if data[0]:
+        cv2.imshow("asdf",data[1])
+        cv2.waitKey(0)
+        request = requests.post("localhost", data=io.BytesIO(data[1]))
+    cv2.destroyAllWindows()
