a89982097ccdecd1904f7a97b28451ab93d54bc0
Sawyer Hoeschele <56524419+sawyer42@users.noreply.github.com>
2024-11-09 19:56:21 -0500

    Added motion processing.
    Still need to make it save in .PIL format tho

--- Image Processing/Object Detection.py
+++ Image Processing/Object Detection.py
@@ -5,17 +5,35 @@ import sys
 
 cap = cv.VideoCapture(1)
 
-threshold = 10
+std_threshold = 10
 
 count = 0
+
+ctime = time.time()
+while time.time() < ctime + 2:
+    cap.read()
+
+print('timeout past')
+
+ret, last = cap.read()
+
+in_motion = False
 
 while True:
     ret, frame = cap.read()
     # convert to a cylindrical-coordinate color mapping system to isolate saturation
     HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     std = np.std(HSV[:,:,1])
-    if std > threshold:
-        cv.imwrite("test.jpg", frame)
-        print("image Saved")
-        count += 1
-    time.sleep(1)
+    diff = np.mean(cv.absdiff(last, frame))
+
+    if diff >= 0.5:
+        in_motion = True
+
+    if in_motion and diff < 0.5:
+        if std > std_threshold:
+            cv.imwrite("test.jpg",frame)
+            print('Image saved')
+        in_motion = False
+    
+    last = frame
+    time.sleep(0.2)
